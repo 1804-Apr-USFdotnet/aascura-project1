979fbbf0ab572da115024ff2b557945889e1473d
Ascura-Austin <austin.ascura@gmail.com>
2018-04-23 11:06:43 -0400

    adding base work.  tests in place, working on punctuation handling

---
+++ Week1CodeChallenge/LibraryCodeChallenge/Palindrome.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibraryCodeChallenge
+{
+    public static class Palindrome
+    {
+        public static bool isValidAlphaNum(string toTest)
+        {
+            bool result = true;
+            string tempCopy = (string) toTest.Clone();
+            int strLen;
+
+            tempCopy = tempCopy.ToUpper();
+
+            strLen = tempCopy.Length;
+            for( int i = 0; i < strLen; i++)
+            {
+                if (tempCopy[i] != tempCopy[strLen - (i + 1)])
+                {
+                    result = false;
+                }
+            }
+
+            return result;
+        }
+    }
+}
---
+++ Week1CodeChallenge/UnitTestCodeChallenge/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("UnitTestCodeChallenge")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("UnitTestCodeChallenge")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("ca38d0d0-a61f-46a6-8f35-0789712884b8")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
