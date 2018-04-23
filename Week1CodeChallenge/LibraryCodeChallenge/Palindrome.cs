1ac84313ceb5f3811aff8853211df2ffdd52ec60
Ascura-Austin <austin.ascura@gmail.com>
2018-04-23 11:20:40 -0400

    almost there.  just finishing up alphanumeric parsing

--- Week1CodeChallenge/LibraryCodeChallenge/Palindrome.cs
+++ Week1CodeChallenge/LibraryCodeChallenge/Palindrome.cs
@@ -8,16 +8,35 @@ namespace LibraryCodeChallenge
 {
     public static class Palindrome
     {
+        static bool IsAlphaNum(char toCheck)
+        {
+            bool result = true;
+            if (false)
+            {
+                result = false;
+            }
+            return result;
+        }
         public static bool isValidAlphaNum(string toTest)
         {
             bool result = true;
             string tempCopy = (string) toTest.Clone();
             int strLen;
+            bool isAlphaNumeric = false;
+            int alphaNumIndex = 0;
+            tempCopy = tempCopy.ToUpper(); //case handling
+            //punctuation handling
 
-            tempCopy = tempCopy.ToUpper();
-
+            for (int i = 0; i < tempCopy.Length; i++)
+            {
+                if (!IsAlphaNum(tempCopy[alphaNumIndex]))
+                {
+                    tempCopy = tempCopy.Remove(toTest[i], 1);
+                    i = 0;
+                }
+            }
             strLen = tempCopy.Length;
-            for( int i = 0; i < strLen; i++)
+            for( int i = 0; i < strLen; i++) //actual comparison (consider cutting in half)
             {
                 if (tempCopy[i] != tempCopy[strLen - (i + 1)])
                 {
