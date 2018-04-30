6145d5737b9740563fbfad857cc86547ab887232
Ascura-Austin <austin.ascura@gmail.com>
2018-04-30 12:37:22 -0400

    Redux.  I definitely goofed but here's the real deal, even if it doesn't count.

--- Project0/codechallenge.sql
+++ Project0/codechallenge.sql
@@ -1,47 +1,57 @@
-CREATE DATABASE code_challenge;
-GO
-
 USE code_challenge;
 GO
 
-CREATE SCHEMA [User];
-GO
-
-CREATE SCHEMA [Content];
-GO
-
-CREATE TABLE [User].[user] 
-( id			INT PRIMARY KEY IDENTITY(0,1)
-, username		CHAR(32)	UNIQUE NOT NULL
-, [password]	CHAR(32)	NOT NULL
-, joinDate		DATETIME2		NOT NULL	
+CREATE TABLE [Products] 
+( ID INT PRIMARY KEY IDENTITY(0,1)
+, [Name] VARCHAR(32) NOT NULL
+, Price FLOAT NOT NULL
 );
 
-CREATE TABLE [Content].[comments]
-( id			INT PRIMARY KEY IDENTITY (0,1)
-, commentText	NVARCHAR(MAX) NOT NULL
-, userId		INT FOREIGN KEY REFERENCES [User].[user](id) NOT NULL
-, [dateTime]	DATETIME2	NOT NULL
+CREATE TABLE [Customers] 
+( ID INT PRIMARY KEY IDENTITY(0,1)
+, Firstname VARCHAR(32) NOT NULL
+, Lastname VARCHAR(32)
+, Cardnumber CHAR(12) NOT NULL
 );
 
-INSERT INTO [User].[user] (username, [password], joinDate) VALUES
-('MasterBlaster','Unsafe Password',GETDATE()),
-('BobSaget', 'password', GETDATE()),
-('NotAGovtAgent', 'bR52[Fsg5m02pQvZ', GETDATE());
+CREATE TABLE [Orders]
+( ID INT PRIMARY KEY IDENTITY(0,1)
+, ProductID INT FOREIGN KEY REFERENCES Products(ID)
+, CustomerID INT FOREIGN KEY REFERENCES Customers(ID)
+);
 
-INSERT INTO [Content].[comments] (commentText, userId, dateTime) VALUES
-('man i love the internet', (SELECT id FROM [User].[user] WHERE [username] = 'MasterBlaster'), GETDATE()),
-('Yes, I too love the internet.  Tell me of your doings.', (SELECT id FROM [User].[user] WHERE [username] = 'NotAGovtAgent'), GETDATE()),
-('I think we may have a plant', (SELECT id FROM [User].[user] WHERE [username] = 'BobSaget'), GETDATE()),
-('whoa are u the real bob saget', (SELECT id FROM [User].[user] WHERE [username] = 'MasterBlaster'), GETDATE())
+INSERT INTO Customers (Firstname, Lastname, Cardnumber) VALUES
+('John', 'Doe', '000000000000' ),
+('Jane', 'Doe', '111111111111' ),
+('Jann', 'Lee', '123456789012' )
 ;
 
-SELECT * FROM [Content].[comments];
+INSERT INTO Products ( [Name], Price) VALUES
+( 'Giant Robot', 999999.99),
+( 'Laptop', 549.83),
+( 'Jeans', 37.48)
+;
 
-SELECT * FROM [Content].comments WHERE userId = (SELECT id FROM [User].[user] WHERE username = 'NotAGovtAgent');
+INSERT INTO Orders (CustomerID, ProductID) VALUES 
+(1, 1),
+(2, 2),
+(3, 3)
+;
 
-UPDATE [User].[user]
-SET username = 'RickAstley' WHERE username = 'BobSaget';
+INSERT INTO Products ( [Name], Price ) VALUES ('iPhone', 200.00);
 
-INSERT INTO [Content].[comments] (commentText, userId, dateTime) VALUES
-('You must be mistaken.', (SELECT id FROM [User].[user] WHERE [username] = 'RickAstley'), GETDATE());
+INSERT INTO Customers (Firstname, Lastname, Cardnumber) VALUES ('Tina', 'Smith', '098765432109');
+
+INSERT INTO Orders (CustomerID, ProductID) VALUES
+( (SELECT ID FROM Customers WHERE Firstname = 'Tina' AND Lastname = 'Smith')
+, (SELECT ID FROM Products WHERE [Name] = 'iPhone')
+);
+
+SELECT o.ID, o.CustomerID, o.ProductID FROM Orders o INNER JOIN (SELECT ID FROM Customers c WHERE Firstname = 'Tina' AND Lastname = 'Smith') sq ON o.CustomerID = sq.ID;
+
+SELECT p.Price FROM Products p WHERE p.Name = 'iPhone';
+
+SELECT SUM(sq.Price) FROM Orders o INNER JOIN (SELECT p.ID, p.Price FROM Products p WHERE p.Name = 'iPhone') sq ON o.ProductID = sq.ID;
+
+UPDATE Products
+SET Price = 250.00 WHERE [Name] = 'iPhone';
